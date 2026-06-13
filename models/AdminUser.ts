5f8645ddc9dd61ea690b7920c8fbe9d21fd3eaef
Tina18082004 <tinamksharma2004@gmail.com>
2026-06-13 21:02:34 +0530

    Fixed AdminUser mongoose hook

--- models/AdminUser.ts
+++ models/AdminUser.ts
@@ -31,10 +31,10 @@ const AdminUserSchema = new Schema<IAdminUser>(
 );
 
 // Hash password before saving
-AdminUserSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
+AdminUserSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
+
   this.password = await bcrypt.hash(this.password, 12);
-  next();
 });
 
 // Compare plain-text password with stored hash
